944b06f57c47f590bcdebd4048ef6967b05a7d1b
Kieran <belli.kieran@gmail.com>
2020-10-26 11:22:28 +0100

    Premier commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from art import *
+
+def say_hello(name):
+    print("hello " + name)
+    print(f"How are you {name} ?")
+n = "bob"
+say_hello(n)
+
+h = art("Hello")
+print(h)# affiche hello
+print(text2art("Hello", font = 'block'))
