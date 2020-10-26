18158716f02fb256bc11e6dfee83ee7bdda0e9bc
Kieran <belli.kieran@gmail.com>
2020-10-26 15:24:47 +0100

    on comprend de mieux en mieux et on classe dans l'ordre

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from art import *
 
-def say_hello(name):
-    print("hello " + name)
-    print(f"How are you {name} ?")
-n = "bob"
-say_hello(n)
 
-h = art("Hello")
-print(h)# affiche hello
+
+def create_contact(name, phone_number, is_favorite):
+    contact = {
+        "Noms": name,
+        "Numeros": phone_number,
+        "Favori": is_favorite,
+              }
+    return contact
+
+carnet = {}
+
+def add_contact(c) :
+        phone_number = c["Numeros"]
+        carnet[phone_number] = c
+
+c = create_contact(name= "Kieran", phone_number="097865434" , is_favorite= True)
+add_contact(c)
+
+c = create_contact( "George", "064333234" ,  False)
+add_contact(c)
+
+def get_names():
+    names = []
+    for k in sorted(carnet):
+        n = carnet[k]["Noms"]
+        print(n)
+    return names
+
+get_names()
+
+
+
+
+
+
+
+
+
+
